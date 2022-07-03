89a86dd4e50e07b66a91243ac5c20c4497547454
Santiago Dubon <santiagodubon@outlook.com>
2022-07-03 16:17:43 -0400

    Add dynamic canvas size

--- js-file.js
+++ js-file.js
@@ -1,23 +1,40 @@
 // ITEMS
 const drawingArea = document.querySelector('.drawing-area')
-let gridSize = 50;
 let grid = 'auto'
 
-drawingArea.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
-drawingArea.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
+const slider = document.querySelector('.slider')
+const sliderText = document.querySelector('.size')
 
-for(let i = 0; i < gridSize * gridSize; i++) {
-    const div = document.createElement('div')
-    div.classList.add('pixel')
-    drawingArea.appendChild(div)
-}
+createDrawingArea(16)
 
-console.log(grid)
+slider.oninput = function() {
+    console.log(slider.value)
+    createDrawingArea(slider.value)
+    sliderText.innerHTML = slider.value + ' x ' + slider.value
+  } 
 
-const pixel = document.querySelectorAll('.pixel')
+function createDrawingArea (gridSize) {
+    drawingArea.removeChild(drawingArea.firstChild)
 
-for (let i = 0; i < pixel.length; i++) {
-    pixel[i].addEventListener('mouseover', () => {
-        pixel[i].style.backgroundColor = 'black'
-    })
+    const canvas = document.createElement('div')
+    canvas.style.display = 'grid'
+
+    canvas.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
+    canvas.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
+
+    for(let i = 0; i < gridSize * gridSize; i++) {
+        const div = document.createElement('div')
+        div.classList.add('pixel')
+        canvas.appendChild(div)
+    }
+
+    drawingArea.appendChild(canvas)
+
+    const pixel = document.querySelectorAll('.pixel')
+
+    for (let i = 0; i < pixel.length; i++) {
+        pixel[i].addEventListener('mouseover', () => {
+            pixel[i].style.backgroundColor = 'black'
+        })
+    }
 }
