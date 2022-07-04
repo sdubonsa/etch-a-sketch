9ae45d550b74d2cc5f0277ec7c602ed9f14fd216
Santiago Dubon <santiagodubon@outlook.com>
2022-07-03 22:02:31 -0400

    Add color picker

--- js-file.js
+++ js-file.js
@@ -5,8 +5,20 @@ let grid = 'auto'
 const slider = document.querySelector('.slider')
 const sliderText = document.querySelector('.size')
 const clrButton = document.querySelector('.clrButton')
+const colorPicker = document.querySelector('.color-picker')
 
 createDrawingArea(16)
+
+// EVENT HANDLERS
+colorPicker.addEventListener('change', () => {
+    const pixel = document.querySelectorAll('.pixel')
+
+    for (let i = 0; i < pixel.length; i++) {
+        pixel[i].addEventListener('mouseover', () => {
+            pixel[i].style.backgroundColor = colorPicker.value
+        })
+    }
+})
 
 slider.oninput = function() {
     console.log(slider.value)
@@ -18,6 +30,7 @@ clrButton.onclick = function() {
     createDrawingArea(slider.value)
 }
 
+// FUNCTIONS
 function createDrawingArea (gridSize) {
     drawingArea.removeChild(drawingArea.firstChild)
 
@@ -39,7 +52,7 @@ function createDrawingArea (gridSize) {
 
     for (let i = 0; i < pixel.length; i++) {
         pixel[i].addEventListener('mouseover', () => {
-            pixel[i].style.backgroundColor = 'black'
+            pixel[i].style.backgroundColor = colorPicker.value
         })
     }
 }
